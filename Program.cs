1ee2b6d59fbce8689873b8a92e02e25173e11935
Major-Cod3 <ricardodejesusbarbosa4@gmail.com>
2026-01-06 16:59:12 -0300

    Adiciona informações de artista e gênero no programa; gera arquivos de configuração do projeto ScreenSound2

--- Program.cs
+++ Program.cs
@@ -1,19 +1,21 @@
-﻿Genero genero = new Genero();
+﻿Artista artista1 = new Artista("Baco Exu do Blues");
+Console.WriteLine(artista1.Nome);
+Genero genero = new Genero();
 genero.Nome = "Rap";
 
 Album album = new Album();
 album.Titulo = "QVVJFA?";
 album.Artista = "Baco Exu do Blues";
 
-Musica musica1 = new Musica();
+Musica musica1 = new Musica( artista1);
 musica1.Nome = "20 ligacoes";
-musica1.Artista = "Baco Exu do Blues";
+
 
 musica1.Genero = genero;
 musica1.Duracao = 180;
-Musica musica2 = new Musica();
+Musica musica2 = new Musica(artista1);
 musica2.Nome = "Sulicidio";
-musica2.Artista = "Baco Exu do Blues";
+
 musica2.Duracao = 200;
 musica2.Genero = genero;
 
@@ -21,11 +23,8 @@ album.AddMusica(musica1);
 album.AddMusica(musica2);
 Genero genero1 = new Genero();
 genero1.Nome = "blues";
-Banda banda = new Banda();
-banda.Nome = "999";
-banda.AddGenero(genero);
-banda.AddGenero(genero1);
-banda.AddAlbum(album);
-banda.AddMusica(musica1);
-banda.AddMusica(musica2);
-banda.MostrarInfo();
+artista1.AddAlbum(album);
+artista1.AddMusica(musica1);
+artista1.AddMusica(musica2);
+artista1.MostrarInfo();
+
